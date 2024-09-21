9d462f116547c7aaea86a98a3fb32ac79c9ac45a
tahaunalll <tahaunal@uludag.edu.tr>
2024-09-22 01:39:14 +0300

    ngSide1st

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginModel } from '../../models/login.model';
+import { FormValidateDirective } from 'form-validate-angular';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    FormsModule,
+    FormValidateDirective
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  login : LoginModel = new LoginModel();
+@ViewChild("password") password : ElementRef<HTMLInputElement>|undefined;
+
+showOrHidePassword(){
+  if(this.password === undefined) return ;
+  if(this.password.nativeElement.type==="password"){
+    this.password.nativeElement.type="text";
+  } else {
+    this.password.nativeElement.type ="password";
+  }
+  
+}
+
+
+}
